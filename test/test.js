61a8344d89f18e45d6bbab4f46b7e58209fdb454
Howard <nnt18119@hotmail.com>
2017-12-22 18:31:28 +0800

    add a error test case

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var assert = require("assert");
 describe("Array", function(){
     describe("#indexOf()", function(){
         it('should return -1 when value is not present', function(){
-            assert.equal(-1, [1,2,3].indexOf(0));
+            assert.equal(3, [1,2,3].indexOf(0));
         })
     })
 })
